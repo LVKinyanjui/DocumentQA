4c9be2af387a6e5197b19c35a5b05d5b3153af3e
LVKinyanjui <leslievictorius@gmail.com>
2024-01-11 19:43:40 +0300

    Added launch files for huggingface

---
+++ app.py
@@ -0,0 +1,22 @@
+# %%
+import gradio as gr
+from auxiliaries import embed_upsert, retrieve
+
+# %%
+with gr.Blocks() as iface:
+    namespace = gr.Textbox()
+
+    gr.ChatInterface(
+        fn=retrieve, 
+        additional_inputs=[namespace]
+    ).launch()
+
+    file = gr.File()
+    feedback = gr.Markdown()
+
+    file.change(fn=embed_upsert, inputs=file, outputs=[feedback, namespace])
+
+# %%
+iface.launch()
+
+
