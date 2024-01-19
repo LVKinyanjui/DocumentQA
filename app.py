819786b02ce352eae7aeffe91cff7394ab5f54cb
LVKinyanjui <leslievictorius@gmail.com>
2024-01-19 23:16:28 +0300

    Added an experimental summary feature: needs optimization

--- app.py
+++ app.py
@@ -1,44 +1,31 @@
 # %%
 import gradio as gr
-from auxiliaries import embed_upsert, retrieve
+from auxiliaries import embed_upsert, retrieve, summarize
 
 # %%
 with gr.Blocks() as iface:
     namespace = gr.Textbox(
-        label="Namespace", 
-        placeholder="Display or Enter Pinecone Document Namespace",
-        visible=False)
-
-    gr.ChatInterface(
-        fn=retrieve, 
-        additional_inputs=[namespace]
-    )
-
-    file = gr.File()
-    feedback = gr.Markdown()
-
-    file.change(fn=embed_upsert, inputs=file, outputs=[feedback, namespace])
-
-# %%
-namespace = gr.Textbox(
     label="Namespace (Pinecone)", 
     placeholder="(Optional) Display or Enter Pinecone Document Namespace",
+    visible=False
     )
-
-with gr.Blocks() as iface:
 
     gr.ChatInterface(
         fn=retrieve, 
         additional_inputs=[namespace]
     )
 
+    with gr.Accordion("Document Summary"):
+        summary = gr.Markdown()
+
     file = gr.File()
     feedback = gr.Markdown()
 
     file.change(fn=embed_upsert, inputs=file, outputs=[feedback, namespace])
+    file.change(fn=summarize, inputs=file, outputs=summary)
 
 # %%
-iface.launch()
+iface.launch(share=True)
 
 # %%
 
